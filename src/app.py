5f4a23a0cec05267f84348a386c1de54da5e3801
Haniel Costa da Silva <hanielcosta.ti@gmail.com>
2024-05-09 11:49:44 -0300

    mudanca em nome de variáveis

--- src/app.py
+++ src/app.py
@@ -1,3 +1,5 @@
+import os
+from openpyxl import load_workbook
 from openpyxl.styles import PatternFill
 import tableformat as tf
 from autom import autom
@@ -28,18 +30,27 @@ green = PatternFill(start_color="40DE47", end_color="40DE47", fill_type="solid")
 continue_autom= False
 
 for index,row in fdf.func_situacao_df.iterrows():
-    continue_autom = autom.confirm(f"Deseja realizar o cadastro de \n {row["NOME"]}?")
+    
+    situacao = row["SITUACAO"]
+    matricula = row["MATRICULA"]
+    nome = row["NOME"] 
+
+    #Caso não esteja PENDENTE a situação do funcionario
+    if situacao != fdf.situacao_lista[0]:
+        print(f"foi pulado o funcionário: {nome}\n Matricula:{matricula}, Possuindo a seguinte situação: {situacao}")
+        continue
+    
+    continue_autom = autom.confirm(f"Deseja realizar o cadastro de \n {row['NOME']}?")
     
     if not continue_autom:
         if autom.confirm(f"Encerrar o programa? "): break
     
-    matricula = row["MATRICULA"]
-    nome = row["NOME"] 
-    #MOVE O CURSOR PARA A ENTRADA DA MATRICULA
+
+    #MOVE O CURSOR PARA O INPUT DA MATRICULA
     autom.moveCursor(moves[0][0], moves[0][1])
     autom.click(3)
     
-    #ESCREVA A ENTRADA DA MATRICULA
+    #ESCREVA O INPUT DA MATRICULA
     autom.write(matricula)
     
     #CONFIRME
@@ -50,7 +61,7 @@ for index,row in fdf.func_situacao_df.iterrows():
     
     while problema_ocorrido in ["","CANCEL"]:
     
-        if autom.custom_message("Aconteceu algum erro? ",[ "sim", "não"]) == "sim":
+        if autom.custom_message("Conseguiu Acesso ao Portal do Funcionario? ",[ "sim", "não"]) == "sim":
             
             problema_ocorrido = autom.custom_message("Informe qual o problema: ", problems + ["ADICIONAR"])
             
@@ -59,8 +70,34 @@ for index,row in fdf.func_situacao_df.iterrows():
                 
                 if problema_ocorrido not in problems: 
                     problems.append(problema_ocorrido)
-            
+                
         else:
             break
-              
+  
+fdf.save_func_situacao_df()     
+# Abre o arquivo Excel
+actual_directory = os.path.dirname(os.path.realpath(__file__))
+
+final_file_path = os.path.join(actual_directory,"..","data","final_register")
+
+workbook = load_workbook(final_file_path)
+sheet = workbook.active
+
+# Percorre o DataFrame e aplica a formatação às células correspondentes
+for index, row in fdf.func_situacao_df.iterrows():
+    situacao = row["SITUACAO"]
+    matricula = row["MATRICULA"]
+    nome = row["NOME"] 
+    
+    # Verifica a situação e aplica a formatação adequada
+    if situacao == "RENOMEADO":
+        tf.paintRow(sheet, index + 2, yellow)  # +2 para ajustar o índice da linha
+    elif situacao == "NAO NECESSARIO":
+        tf.paintRow(sheet, index + 2, orange)
+    elif situacao == "ERRO":
+        tf.paintRow(sheet, index + 2, green)
+
+# Salva as alterações no arquivo Excel
+workbook.save(final_file_path)
+
 autom.alert("Programa encerrado")
--- src/dataframes/funcionario.py
+++ src/dataframes/funcionario.py
@@ -8,14 +8,18 @@ actual_directory = os.path.dirname(os.path.realpath(__file__))
 
 func_path = os.path.join(actual_directory,"..","..","data","func.xlsx")
 
-func_log_path = os.path.join(actual_directory,"..","..","data","func_log.xlsx")
+func_situacao_path = os.path.join(actual_directory,"..","..","data","func_log.xlsx")
 
 # == == Lista
 situacao_lista = ["PENDENTE","ALTERADO"]
 #PENDENTE = ainda não foi checado a situação do Funcionario
 #ALTERADO = O nome do funcionário foi alterado  
 
-def get_func_original_df(path):
+def save_func_situacao_df():
+    func_situacao_df.to_excel(func_situacao_path,index=False)
+    
+
+def get_func_original_df(path:str):
     func_df =pd.read_excel(path)
     return func_df
 
@@ -25,7 +29,7 @@ def create_func_situacao_df(func_df:'pd.DataFrame'):
     #Adicionamos a coluna SITUACAO no final da tabela 
     func_situacao_df["SITUACAO"] = situacao_lista[0]
     #Criamos a planilha na pasta data
-    func_situacao_df.to_excel(func_log_path,index=False)
+    func_situacao_df.to_excel(func_situacao_path,index=False)
     
     return func_situacao_df
     
@@ -41,9 +45,9 @@ func_orginal_df =get_func_original_df(func_path)
 
 func_situacao_df = None
 
-if os.path.exists(func_log_path):
+if os.path.exists(func_situacao_path):
     # Carregue os cabeçalhos da planilha existente
-    func_situacao_df = get_func_situacao(func_log_path)
+    func_situacao_df = get_func_situacao(func_situacao_path)
        
 else: 
     func_situacao_df = create_func_situacao_df(func_orginal_df)
