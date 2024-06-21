24928a3b632ffdf8dc11380235b972de37dee617
Haniel Costa da Silva <hanielcosta.ti@gmail.com>
2024-06-21 12:01:11 -0300

    mudanças no movimento da automatização  e cores

--- src/app.py
+++ src/app.py
@@ -8,7 +8,6 @@ from autom import autom
 
 import model
 
-
 def finalizar_autom():    
     resposta=""
     while resposta =="":
@@ -50,7 +49,6 @@ def get_problem(text:str="")->str:
             
         elif escolha == "LIMPAR": #caso ele tenha escolhido limpar os possíveis problemas
             problems.clear()
-            problems.append("MATRICULA INEXISTENTE")
             continue        
         
         elif escolha =="": #Caso ele tenha fechado a janela, refaça a pergunta
@@ -95,13 +93,13 @@ def confirm_acess(func:Funcionario)-> bool:
 
 #== == == Variables
 
-
 #col name
-sit_col ="SITUACAO_DF"
+sit_DF_col ="SITUACAO_DF"
+UNO_col ="UNO"
+sit_col ="SITUAÇÃO"
 
 #== Path DataFrame
 actual_directory = os.path.dirname(os.path.realpath(__file__))
-
 
 func_situacao_path = os.path.join(actual_directory,"..","data","func_log.xlsx")
 
@@ -110,31 +108,31 @@ func_situacao_df = model.Dataframe(func_situacao_path)
 
 # == == autom
 #moves_to_form
-moves=[ (1124,849), (232,288)]
+moves_tela_1=[ (560,850), (1312,850), (230,300)]
+moves_tela_2=[ (2390,800), (3011,800),(2100,360)]
 
-#quanty_enter_to_confirm
-quanty_enters=3
+# 1º Option Input
+# 2º Enrollment Input
+# 3º Name Input
+
+#options
+options=["02"]
+
+moves = moves_tela_1
+
+quanty_press_confirm=3 #quanty_time_press_enter_to_confirm
+quanty_press_return=3 #quanty_time_press_f3_to_return
 
 # == == Lista
 
 #== == possibles_problem
-problems=["MATRICULA INEXISTENTE"]
+problems=[]
 
 #== == situacao
 situacao_lista = ["PENDENTE","INALTERADO","ALTERADO"]
 #PENDENTE = ainda não foi checado a situação do Funcionario
 #ALTERADO = O nome do funcionário não foi alterado  
 #ALTERADO = O nome do funcionário foi alterado  
-
-# == == Collors
-#AMARELO quando o funcionário foi devidamente renoemado 
-yellow = PatternFill(start_color="FFFF00", end_color="FFFF00", fill_type="solid")
-
-#LARANJA quando não foi necessário renomear
-orange = PatternFill(start_color="EC9006", end_color="EC9006", fill_type="solid")
-
-#VERDE quando ocorreu um erro ao tentar renomear o funcionário
-green = PatternFill(start_color="40DE47", end_color="40DE47", fill_type="solid")
 
 continue_autom= False
 
@@ -152,32 +150,38 @@ if len(func_situacao_df) ==0:
     for _ , func in func_df.get_iterrows():
         func_situacao_df.add_row(**func)
     
-    func_situacao_df.add_column(sit_col,situacao_lista[0])
+    func_situacao_df.add_column(sit_DF_col,situacao_lista[0])
     func_situacao_df.save()
     
     
 autom.alert("Programa Inciado. Por favor, conecte-se a sua conta")
 
+#O usuário deverá escolher em qual tela o SISTEMA 
+tela_sis=""
+telas_escolha=["1º Monitor","2º Monitor"]
+
+while tela_sis=="":
+    tela_sis= autom.custom_message("Em qual monitor está o Sistema?", telas_escolha)
+
+moves = moves_tela_1 if tela_sis == telas_escolha[0] else moves_tela_2
 try:
 
-    for index,situacao in func_situacao_df.get_columns(sit_col):
-        
+    for index,situacao in func_situacao_df.get_columns(sit_DF_col):
+                
         row = func_situacao_df[index]
 
-        #situacao = row["SITUACAO"]
         matricula = row["MATRICULA"]
-        nome = row["NOME"] 
+        nome_alterar = row["COD_CPF_NOME"] 
         cpf = row["CPF"]
-        setor = row["SETOR"] 
-        func= Funcionario(matricula,nome,setor,cpf,sit_col)
+ 
+        func= Funcionario(matricula,nome_alterar,'',cpf,situacao)
         
         #Caso não esteja PENDENTE a situação do funcionario
         if situacao != situacao_lista[0]:
             print("=="*30)
             print(f"Pulei o funcionário: {func.matricula} : {func.nome}")
             print("=="*30)
-            continue
-        
+            continue        
         #pergunte se deseja continuar a automoção
         continue_autom = False
     
@@ -186,16 +190,18 @@ try:
 
             if not continue_autom:
                 finalizar_autom()
-        """ 
-        if not continue_autom:
-            break 
-        """    
 
-        #MOVE O CURSOR PARA O INPUT DA MATRICULA
-        autom.moveCursor(moves[0][0], moves[0][1])
+        #MOVE O CURSOR PARA O INPUT DA OPCAO
+        autom.moveCursor(*moves[0])
         autom.click()
-        #APAGUE O CONTEUDO ANTERIOR
-        #autom.press("del")        
+        #ESCREVA A OPÇÃO DE ALTERAR
+        autom.click()
+        autom.write(options[0])
+        
+        #MOVE O CURSOR PARA O INPUT DA MATRICULA
+        autom.moveCursor(*moves[1])
+        autom.click()
+
         #ESCREVA O INPUT DA MATRICULA
         autom.write(matricula)
         
@@ -223,20 +229,25 @@ try:
             needs_alter = ask_nedds_alter(func) 
             if needs_alter:  
                 #Continue com a automação
-                autom.moveCursor(moves[1][0], moves[1][1]) #mova o cursor para o input de nome
+                autom.moveCursor(*moves[2]) #mova o cursor para o input de nome
                 autom.click()
-                #autom.press("del")        
-                #autom.click()
 
-                autom.write(nome)
-                autom.press_enter(quanty_enters) #preciona a quantidade de enters necessárias para confirmar
+                autom.write(nome_alterar)
+                autom.press_enter(quanty_press_confirm) #preciona a quantidade de enters necessárias para confirmar
                 
                 problem = get_problem(f"{func}\n\n Você alterou o nome do funcionário")
-        
                 if problem != "":
                     #foi detectado algum erro ao tentar renomear o usuário
                     situacao= problem
-                
+                    
+                    #peça o codigo do UNO
+                    UO_codigo=autom.prompt("Digite o codigo da UNIDADE ORCAMENTARIA (UNO) do funcionário")
+                    func_situacao_df.alter_column_index(index,UNO_col,UO_codigo)
+                    #peça o codigo da situacao
+                    situacao_cod=autom.prompt("Digite o codigo da SITUAÇÃO do funcionário")
+                    #altere no dataframe
+                    func_situacao_df.alter_column_index(index,sit_col,situacao_cod)
+                    autom.press("f3",quanty_press_return)
                 else:
                     situacao = situacao_lista[-1]     
                     autom.press_enter(1)
@@ -246,10 +257,9 @@ try:
                 situacao = situacao_lista[1]
         
         #Por fim, alteramos a situação no DF
-        func.situacao=situacao        
-        func_situacao_df.alter_column_index(index,sit_col,situacao)
+        func_situacao_df.alter_column_index(index,sit_DF_col,situacao)
        
-        print(f"M {func.matricula} => Nome {func.nome}, Sit. {func.situacao}")
+        print(f"M {matricula} => Nome {nome_alterar}, Sit. {situacao}")
 
 except autom.AutomException as AE:
     print(AE)
@@ -267,7 +277,17 @@ func_situacao_df.save()
 workbook = load_workbook(func_situacao_df.path)
 ws = workbook.active
 
-sit_df_position = func_situacao_df.columns.get_loc(sit_col)
+sit_df_position = func_situacao_df.columns.get_loc(sit_DF_col)
+
+# == == Collors
+#AMARELO quando o funcionário foi devidamente renoemado 
+yellow = PatternFill(start_color="FFFF00", end_color="FFFF00", fill_type="solid")
+
+#LARANJA quando não foi necessário renomear
+orange = PatternFill(start_color="EC9006", end_color="EC9006", fill_type="solid")
+
+#VERDE quando ocorreu um erro ao tentar renomear o funcionário
+red = PatternFill(start_color="FF4200", end_color="FF4200", fill_type="solid")
 
 for row in ws.iter_rows(min_row=2, max_row = len(func_situacao_df) + 1, min_col=1, max_col=len(func_situacao_df.columns)):
 
@@ -276,7 +296,6 @@ for row in ws.iter_rows(min_row=2, max_row = len(func_situacao_df) + 1, min_col=
     if situacao_func == situacao_lista[-1]: #ALTERADO
         tf.paintRow(row,yellow)
         
-        
     elif situacao_func == situacao_lista[1]: #INALTERADO
         tf.paintRow(row,orange)
         
@@ -284,7 +303,7 @@ for row in ws.iter_rows(min_row=2, max_row = len(func_situacao_df) + 1, min_col=
         continue
     
     else: #Ocorreu algum problema
-        tf.paintRow(row,green)
+        tf.paintRow(row,red)
     
 # Salva as alterações no arquivo Excel
 workbook.save(func_situacao_df.path)
